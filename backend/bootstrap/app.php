3923978ae0e1af4c7d9ca8e56bee0e87970f75d5
hafidnm <mahmudi.hafid27@gmail.com>
2026-03-15 13:36:57 +0700

    testing postman

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->validateCsrfTokens(except: [
         'api/login',
         'api/register',
+        'api/logout',
     ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
