3fa42bece2c000b9dfdf277f8ae262ba110e8911
hafidnm <mahmudi.hafid27@gmail.com>
2026-05-18 16:42:05 +0700

    update

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,7 +86,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1') . (env('DB_OPTIONS') ? ';options=' . env('DB_OPTIONS') : ''),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
